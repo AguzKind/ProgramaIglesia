47963654d8167c75f4e5238e7b1178e48ee8fa8d
Agustin Macoggi <105683402+AguzKind@users.noreply.github.com>
2023-05-08 17:42:25 -0300

    Carga archivos (pt3)

---
+++ Prj_Trabajo/Reportes_Comuniones.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prj_Trabajo
+{
+    public class Reportes_Comuniones
+    {
+        public string Fecha { get; set; }
+        public string Hora { get; set; }
+    }
+}
---
+++ Prj_Trabajo/Reportes_Misas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prj_Trabajo
+{
+    public class Reportes_Misas
+    {
+        public string Fecha { get; set; }
+        public string Hora { get; set; }
+        public string Intencion { get; set; }
+    }
+}
